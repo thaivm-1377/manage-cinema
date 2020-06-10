226b1ac60e2d5bd0e2ead0098d841a71bec0c10a
thaivm-1377 <vuong.minh.thai@sun-asterisk.com>
2020-06-10 10:42:37 +0700

    update something

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,45 +27,4 @@ class HomeController extends Controller
     {
             return view('auth.login');
     }
-
-    // public function adminLogin(){
-    //     $reviews = $this->reviewRepository->paginateHome();
-    //     $rateReviewVals = $this->reviewRepository->listReviewVal();
-    //     $rateReview = $this->rateRepository->findRateLike();
-    //     if (Auth::check()) {
-    //         $userId = Auth::user()->id;
-    //         foreach ($reviews as $review) {
-    //             $countLike[$review->id] = $this->rateValRepository->getLikes($review->id);
-    //             $countComment[$review->id] = $this->commentRepository->getCommentNumber($review->id);
-    //             $hasLike[$review->id] = $this->rateValRepository->findReviewID($review->id, $userId);
-    //         }
-
-    //         foreach ($reviews as $review) {
-    //             $countLike[$review->id] = $this->rateValRepository->getLikes($review->id);
-    //             $countComment[$review->id] = $this->commentRepository->getCommentNumber($review->id);
-    //         }
-    
-    //         return view('frontend.index', compact(
-    //             'reviews',
-    //             'rateReviewVals',
-    //             'countLike',
-    //             'rateReview',
-    //             'countComment',
-    //             'hasLike'
-    //         ));
-    //     } else {
-    //         foreach ($reviews as $review) {
-    //             $countLike[$review->id] = $this->rateValRepository->getLikes($review->id);
-    //             $countComment[$review->id] = $this->commentRepository->getCommentNumber($review->id);
-    //         }
-    
-    //         return view('frontend.index', compact(
-    //             'reviews',
-    //             'rateReviewVals',
-    //             'countLike',
-    //             'rateReview',
-    //             'countComment'
-    //         ));
-    //     }   
-    // }
 }
