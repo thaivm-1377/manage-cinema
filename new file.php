9cfaea288d6ed8ab4c05cc6a50d99c3caf3600b6
thaivm-1377 <49258631+thaivm-1377@users.noreply.github.com>
2020-06-10 15:42:07 +0700

    Update new file.php

--- new file.php
+++ new file.php
@@ -30,10 +30,16 @@ class HandleHook extends Controller
             ->where('scm', $scm)
             ->first();
 
+        
+        
+        
         if (!$repo) {
             abort(response('Repository not found!', 400));
         }
 
+        
+        
+        
         try {
             $webhookPayload = $this->gitService
                 ->webhooks($scm)
